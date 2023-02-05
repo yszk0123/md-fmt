cb31be66bdb0f0cf286e44b00760c77e5594274c
Yuji Suzuki <y0suzuki0123@gmail.com>
2023-02-05 18:52:11 +0900

    Extract run_file

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ pub mod cli;
 mod note;
 
 use std::fs;
+use std::path::PathBuf;
 
 use anyhow::{anyhow, Context, Result};
 use markdown::mdast::Node;
@@ -37,46 +38,52 @@ pub fn format(node: &Node) -> Result<String> {
     NotePrinter::print(&note)
 }
 
-pub fn run(config: Config) -> Result<()> {
-    for file in config.files {
-        let content = fs::read_to_string(&file)
-            .with_context(|| format!("could not read file `{}`", file.display()))?;
-
-        if config.check {
-            let ok = to_mdast_from_str(&content)
-                .and_then(|node| format(&node))
-                .is_ok();
-            println!("{ok}: \"{}\"", file.display());
-            continue;
-        }
-
-        let node = to_mdast_from_str(&content)
-            .with_context(|| format!("could not parse file `{}`", file.display()))?;
-
-        if config.md {
-            let s = pretty(&node);
-            println!("{s}");
-            return Ok(());
-        }
-
-        if config.note {
-            let note = NoteParser::parse(&node)?;
-            let s = note::pretty::pretty(&note);
-            println!("{s}");
-            return Ok(());
-        }
-
-        let content = format(&node)
-            .with_context(|| format!("could not stringify file `{}`", file.display()))?;
-
-        if config.write {
-            fs::write(&file, content)
-                .with_context(|| format!("could not write file `{}`", file.display()))?;
-            return Ok(());
-        }
-
-        println!("{content}");
+pub fn run(config: &Config) -> Result<()> {
+    for file in config.files.iter() {
+        run_file(config, file)?;
     }
+
+    Ok(())
+}
+
+fn run_file(config: &Config, file: &PathBuf) -> Result<()> {
+    let content = fs::read_to_string(file)
+        .with_context(|| format!("could not read file `{}`", file.display()))?;
+
+    if config.check {
+        let ok = to_mdast_from_str(&content)
+            .and_then(|node| format(&node))
+            .is_ok();
+        println!("{ok}: \"{}\"", file.display());
+        return Ok(());
+    }
+
+    let node = to_mdast_from_str(&content)
+        .with_context(|| format!("could not parse file `{}`", file.display()))?;
+
+    if config.md {
+        let s = pretty(&node);
+        println!("{s}");
+        return Ok(());
+    }
+
+    if config.note {
+        let note = NoteParser::parse(&node)?;
+        let s = note::pretty::pretty(&note);
+        println!("{s}");
+        return Ok(());
+    }
+
+    let content =
+        format(&node).with_context(|| format!("could not stringify file `{}`", file.display()))?;
+
+    if config.write {
+        fs::write(file, content)
+            .with_context(|| format!("could not write file `{}`", file.display()))?;
+        return Ok(());
+    }
+
+    println!("{content}");
 
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use mdfmt::cli::config::Config;
 fn main() -> Result<()> {
     let config = Config::build(env::args())?;
 
-    mdfmt::run(config)?;
+    mdfmt::run(&config)?;
 
     Ok(())
 }
