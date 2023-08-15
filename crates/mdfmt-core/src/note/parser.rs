da3782f26794e5b2db0a88d4d9fdcc612a13fe97
Yuji Suzuki <y0suzuki0123@gmail.com>
2023-08-15 15:17:40 +0900

    Fix lint errors

--- crates/mdfmt-core/src/note/parser.rs
+++ crates/mdfmt-core/src/note/parser.rs
@@ -175,9 +175,7 @@ impl NoteParser {
     // > content
     fn parse_card_paragraph(&self, value: &str) -> Option<(NoteKind, String)> {
         let mut lines = value.lines();
-        let Some(v) = lines.next() else { return None };
-
-        match v {
+        match lines.next()? {
             "[!note]" => Some((NoteKind::Note, lines.join("\n"))),
             "[!question]" => Some((NoteKind::Question, lines.join("\n"))),
             "[!quote]" => Some((NoteKind::Quote, lines.join("\n"))),
