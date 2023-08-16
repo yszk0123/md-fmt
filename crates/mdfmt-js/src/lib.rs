928fec5bae0b4cde20e96faeb73ebc312bc022a6
Yuji Suzuki <y0suzuki0123@gmail.com>
2023-08-16 22:33:50 +0900

    Fix export type

--- crates/mdfmt-js/src/lib.rs
+++ crates/mdfmt-js/src/lib.rs
@@ -1,13 +1,26 @@
-use mdfmt_core as core;
+use mdfmt_core;
 pub use mdfmt_core::model;
+use serde_wasm_bindgen;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn format(input: &str) -> String {
-    core::format(input).unwrap_or_else(|_| input.to_string())
+    mdfmt_core::format(input).unwrap_or_else(|_| input.to_string())
 }
 
 #[wasm_bindgen]
-pub fn parse(input: &str) -> Option<Note> {
-    core::parse(input).ok()
+extern "C" {
+    #[wasm_bindgen(typescript_type = "Note")]
+    pub type Note;
+}
+
+#[wasm_bindgen]
+pub fn parse(input: &str) -> Result<Note, JsError> {
+    let note = mdfmt_core::parse(input).map_err(to_js_error)?;
+    let value = serde_wasm_bindgen::to_value(&note).map_err(to_js_error)?;
+    Ok(Note::from(value))
+}
+
+fn to_js_error(err: impl std::fmt::Display) -> JsError {
+    JsError::new(&err.to_string())
 }
