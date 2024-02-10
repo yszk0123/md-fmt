94361d508ba220833721328f6f58ab9fe245d3e0
Yuji Suzuki <y0suzuki0123@gmail.com>
2024-02-10 17:32:09 +0900

    Simplify bundle process

--- crates/mdfmt-js/main.js
+++ crates/mdfmt-js/main.js
@@ -1,13 +1,8 @@
 import wasm from "./pkg/mdfmt_js_bg.wasm";
-import { __wbg_set_wasm } from "./pkg/mdfmt_js_bg.js";
-import * as bg from "./pkg/mdfmt_js_bg.js";
+import loadWasm from "./pkg/mdfmt_js.js";
 
-export * from "./pkg/mdfmt_js_bg.js";
+export * from "./pkg/mdfmt_js.js";
 
 export async function load() {
-    const buf = Buffer.from(wasm, "base64");
-    const res = await WebAssembly.instantiate(buf, {
-        "./mdfmt_js_bg.js": bg,
-    });
-    __wbg_set_wasm(res.instance.exports);
+    await loadWasm(Buffer.from(wasm, "base64"));
 }
